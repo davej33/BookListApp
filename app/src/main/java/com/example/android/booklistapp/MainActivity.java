56663dc1ad7f95e1949276fce52413307dae6448
David Justen <davejusten03@yahoo.com>
2016-11-02 11:06:12 -0400

    search button switches to list view

--- app/src/main/java/com/example/android/booklistapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.android.booklistapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mSearchInput;
+    private BookAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText search_parameters = (EditText) findViewById(R.id.search_parameter);
+        mSearchInput = search_parameters.getText().toString();
+
+        Button search_button = (Button) findViewById(R.id.search_button);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.list_view);
+
+                ListView listView = (ListView) findViewById(R.id.list_view);
+                mAdapter = new BookAdapter(MainActivity.this, new ArrayList<Book>());
+                listView.setAdapter(mAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+                });
+            }
+        });
     }
 }
